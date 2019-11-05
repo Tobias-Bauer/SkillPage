ed8e07fc91295683af48d176ef524dce272463f5
Tobias Bauer <tobiasbauer@Tobiass-MBP.fritz.box>
2019-11-05 19:17:38 +0100

    first commit

---
+++ precache-manifest.f264a49b4da2d2163a5dc286d8d9c065.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "34066be516e25f9e88fc",
+    "url": "/Tobias-Bauer/MyProfilePage/static/css/main.9c5f296d.chunk.css"
+  },
+  {
+    "revision": "34066be516e25f9e88fc",
+    "url": "/Tobias-Bauer/MyProfilePage/static/js/main.b38bbd1a.chunk.js"
+  },
+  {
+    "revision": "7cd40da4113746fa9e4d",
+    "url": "/Tobias-Bauer/MyProfilePage/static/js/runtime~main.a92976e0.js"
+  },
+  {
+    "revision": "68cece187c6e1a0adffd",
+    "url": "/Tobias-Bauer/MyProfilePage/static/js/2.2270012a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tobias-Bauer/MyProfilePage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b4f48c05809019682dfa656294ee8af9",
+    "url": "/Tobias-Bauer/MyProfilePage/static/media/logo-dark.b4f48c05.svg"
+  },
+  {
+    "revision": "eeb5ba31453dd03289e8efac9ce8321b",
+    "url": "/Tobias-Bauer/MyProfilePage/static/media/car.eeb5ba31.svg"
+  },
+  {
+    "revision": "f56d53f55da2397875849dbfc5883d64",
+    "url": "/Tobias-Bauer/MyProfilePage/index.html"
+  }
+];
